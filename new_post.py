244972819c3faa4298bacec7d51e9754831a4f87
Jay Miller <kjaymiller@gmail.com>
2023-01-12 19:30:09 -0800

    start of micropost

---
+++ new_post.py
@@ -0,0 +1,26 @@
+import typer
+from pathlib import Path
+from datetime import datetime
+
+
+
+app = typer.Typer()
+
+@app.command()
+def new_post(content: str=typer.Option(..., prompt=True)):
+    date = datetime.now()
+    post_date = date.strftime("%Y-%m-%d %H:%M")
+    title = date.strftime("%Y%m%d%H%MS")
+    path = Path(f"content/microblog/{title}.md")
+
+    template = f"""---
+date: {post_date}
+---
+
+{content}
+"""
+
+    return path.write_text(template)
+
+if __name__ == "__main__":
+    app()
