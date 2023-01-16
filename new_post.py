84faa7512f3ffcae5fbb1d08a2533597d3008ff5
Jay Miller <kjaymiller@gmail.com>
2023-01-16 11:16:29 -0800

    new micropost

--- new_post.py
+++ new_post.py
@@ -10,7 +10,7 @@ app = typer.Typer()
 def new_post(content: str=typer.Option(..., prompt=True)):
     date = datetime.now()
     post_date = date.strftime("%Y-%m-%d %H:%M")
-    title = date.strftime("%Y%m%d%H%MS")
+    title = date.strftime("%Y%m%d%H%M%S")
     path = Path(f"content/microblog/{title}.md")
 
     template = f"""---
