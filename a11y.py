533cc535fa96adbe42b999bba2c78dec0527021e
Jay Miller <kjaymiller@gmail.com>
2023-09-11 08:00:58 -0400

    updates gh workflow

--- a11y.py
+++ a11y.py
@@ -1,14 +1,21 @@
 from playwright.sync_api import sync_playwright
 from axe_playwright_python.sync_playwright import Axe
+import typer 
+import pathlib
 
 axe = Axe()
 
-with sync_playwright() as playwright:
-    browser = playwright.chromium.launch()
-    page = browser.new_page()
-    page.goto("https://kjaymiller.com")
-    results = axe.run(page)
-    browser.close()
 
-print(results.save_to_file(file_path="test-results.json"))
-print(results.generate_report())
+def main(url: str, output: str = "test-results.json") -> None:
+    """Run Axe on a URL and save the results to a file."""
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch()
+        page = browser.new_page()
+        page.goto(url)
+        results = axe.run(page)
+        browser.close()
+
+    pathlib.Path(output).write_text(results.generate_report())
+
+if __name__ == "__main__":
+    typer.run(main)
